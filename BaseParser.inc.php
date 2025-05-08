f0095c92e86afe87028bf32d2ea1553f6a2bb2d9
Jonas Raoni Soares da Silva <github@raoni.org>
2025-05-08 22:28:51 +0300

    #1 Added JATS HTML generator

--- BaseParser.inc.php
+++ BaseParser.inc.php
@@ -14,8 +14,29 @@
 
 namespace PKP\Plugins\ImportExport\ArticleImporter;
 
+use Application;
+use Author;
+use AuthorDAO;
+use DAORegistry;
+use DOMDocument;
+use DOMDocumentType;
+use DOMElement;
+use DOMNode;
+use DOMNodeList;
+use DOMXPath;
+use Exception;
+use GenreDAO;
+use Issue;
+use IssueDAO;
 use PKP\Plugins\ImportExport\ArticleImporter\Exceptions\AlreadyExistsException;
 use PKP\Plugins\ImportExport\ArticleImporter\Exceptions\InvalidDocTypeException;
+use PKPLocale;
+use Publication;
+use PublicFileManager;
+use Section;
+use Submission;
+use SubmissionDAO;
+use XSLTProcessor;
 
 abstract class BaseParser
 {
@@ -29,7 +50,7 @@ abstract class BaseParser
     private $_xpath;
     /** @var int Context ID */
     private $_contextId;
-    /** @var string Default locale, grabbed from the context */
+    /** @var string Default locale, grabbed from the submission or the context's primary locale */
     private $_locale;
     /** @var int[] cache of genres by context id and extension */
     private $_cachedGenres;
@@ -98,11 +119,165 @@ abstract class BaseParser
             $this
                 ->_ensureMetadataIsValidAndParse()
                 ->_ensureSubmissionDoesNotExist()
+                ->_processFullText(true)
                 ->getPublication();
         } catch (\Exception $e) {
             $this->rollback();
             throw $e;
         }
+    }
+
+    private function _processFullText(bool $overwrite = false): static
+    {
+        static $xslt;
+
+        if (!$this->selectFirst('/article/body') || (!$overwrite && count($this->getArticleEntry()->getHtmlFiles()))) {
+            return $this;
+        }
+        libxml_use_internal_errors(true);
+        if (!$xslt) {
+            $document = new DOMDocument('1.0', 'utf-8');
+            $document->load(__DIR__ . '/jats/xslt/main/jats-html.xsl');
+            $xslt = new XSLTProcessor();
+            $xslt->registerPHPFunctions();
+            $xslt->importStyleSheet($document);
+        }
+
+        $metadata = $this->getArticleEntry()->getMetadataFile();
+        $xml = new DOMDocument('1.0', 'utf-8');
+        $xml->load($metadata);
+        $xpath = new DOMXPath($xml);
+        $xpath->registerNamespace('xlink', 'http://www.w3.org/1999/xlink');
+        $defaultLang = $xml->documentElement->getAttributeNS('http://www.w3.org/XML/1998/namespace', 'lang') ?: $this->getLocale();
+        $langs = [$defaultLang => 0];
+        foreach ($this->select("body//sec[@xml:lang!='{$defaultLang}']", null, $xpath) as $sec) {
+            $langs[$sec->getAttributeNS('http://www.w3.org/XML/1998/namespace', 'lang')] = 0;
+        }
+        foreach (array_keys($langs) as $lang) {
+            $xml = new DOMDocument('1.0', 'utf-8');
+            $xml->load($metadata);
+            $xpath = new DOMXPath($xml);
+            $xpath->registerNamespace('xlink', 'http://www.w3.org/1999/xlink');
+
+            foreach ($this->select('//contrib-group/contrib', null, $xpath) as $contribNode) {
+                $affiliations = [];
+                $xrefs = [];
+                /** @var DOMElement */
+                foreach ($contribNode->getElementsByTagName('xref') as $xref) {
+                    $id = $xref->getAttribute('rid');
+                    switch ($xref->getAttribute('ref-type')) {
+                        case 'fn':
+                            /** @var DOMElement $node */
+                            if ($node = $this->selectFirst("//back/fn-group/fn[@id='{$id}']", null, $xpath)) {
+                                $node = $node->cloneNode(true);
+                                /** @var DOMElement */
+                                foreach (iterator_to_array($node->getElementsByTagName('label')) as $label) {
+                                    $label->parentNode->removeChild($label);
+                                }
+                                $this->fixJatsTags($node);
+                                $bioNode = $node->ownerDocument->createElement('bio');
+                                foreach (iterator_to_array($node->childNodes) as $childNode) {
+                                    $bioNode->appendChild($childNode);
+                                }
+                                $xrefs[] = $xref;
+                                $contribNode->appendChild($bioNode);
+                            }
+                            break;
+
+                        case 'aff':
+                            if ($affiliation = preg_replace(['/\r\n|\n\r|\r|\n/', '/\s{2,}/', '/\s+([,.])/'], [' ', ' ', '$1'], trim($this->selectText("../../aff[@id='{$id}']", $xref, $xpath)))) {
+                                $affiliations[] = $affiliation;
+                            }
+                            $xrefs[] = $xref;
+                            break;
+                    }
+                }
+                if (count($affiliations)) {
+                    $node = $contribNode->ownerDocument->createElement('aff', implode('; ', $affiliations));
+                    $contribNode->appendChild($node);
+                }
+                foreach($xrefs as $xref) {
+                    $xref->parentNode->removeChild($xref);
+                }
+            }
+
+            if (count($langs) > 1) {
+                $filter = "body//sec[@xml:lang!='{$lang}']";
+                if ($defaultLang !== $lang) {
+                    $filter .= " | body//sec[not(@xml:lang)]";
+                }
+                $isSharedFnGroup = count($this->select('back/fn-group', null, $xpath)) !== count($langs);
+                if (!$isSharedFnGroup) {
+                    $filter .= " | back/fn-group[@xml:lang!='{$lang}']";
+                    if ($defaultLang !== $lang) {
+                        $filter .= " | back/fn-group[not(@xml:lang)]";
+                    }
+                }
+                foreach (iterator_to_array($this->select($filter, null, $xpath)) as $node) {
+                    $node->parentNode->removeChild($node);
+                }
+            }
+
+            /** @var DOMElement */
+            foreach (iterator_to_array($this->select('body//sec//label', null, $xpath)) as $label) {
+                for($title = $label; ($title = $title->nextSibling) && $title->nodeType !== XML_ELEMENT_NODE;);
+                /** @var DOMElement $title */
+                if ($title && $title->tagName === 'title') {
+                    $title->insertBefore($title->ownerDocument->createTextNode($label->textContent . ' '), $title->firstChild);
+                    $label->parentNode->removeChild($label);
+                }
+            }
+            $switch = function (DOMElement $main, DOMElement $translation): void {
+                $namespace = 'http://www.w3.org/XML/1998/namespace';
+                $mainNodes = iterator_to_array($main->childNodes);
+                $translationNodes = iterator_to_array($translation->childNodes);
+                foreach ($mainNodes as $node) {
+                    $translation->appendChild($node);
+                }
+                foreach ($translationNodes as $node) {
+                    $main->appendChild($node);
+                }
+
+                $translationLang = $translation->parentNode->getAttributeNS($namespace, 'lang');
+                $translation->parentNode->setAttributeNS($namespace, 'lang', $main->getAttributeNS($namespace, 'lang'));
+                $main->setAttributeNS($namespace, 'lang', $translationLang);
+            };
+            /** @var DOMElement */
+            if (
+                ($title = $this->selectFirst("/article/front/article-meta/title-group/article-title[@xml:lang!='{$lang}']", null, $xpath))
+                && ($translatedTitle = $this->selectFirst("/article/front/article-meta/title-group/trans-title-group[@xml:lang='{$lang}']/trans-title", null, $xpath))
+            ) {
+                $switch($title, $translatedTitle);
+            }
+            if (
+                ($title = $this->selectFirst("/article/front/article-meta/title-group/subtitle[@xml:lang!='{$lang}']", null, $xpath))
+                && ($translatedTitle = $this->selectFirst("/article/front/article-meta/title-group/trans-title-group[@xml:lang='{$lang}']/trans-subtitle", null, $xpath))
+            ) {
+                $switch($title, $translatedTitle);
+            }
+            $xml->documentElement->setAttributeNS('http://www.w3.org/XML/1998/namespace', 'lang', $lang);
+
+            /** @var DOMElement */
+            foreach (iterator_to_array($this->select("//xref", null, $xpath)) as $xref) {
+                if (!$this->selectFirst("//[@id='" . $xref->getAttribute('rid') . "']", null, $xpath)) {
+                    echo "Dropped invalid xref to: " . $xref->getAttribute('rid') . "\n";
+                    $xref->parentNode->removeChild($xref);
+                }
+            }
+
+            $output = $xslt->transformToXML($xml);
+
+            if ($output === false) {
+                throw new Exception("Failed to create HTML file from JATS XML: \n" . print_r(libxml_get_errors(), true));
+            }
+            $path = $metadata->getPathInfo() . '/' . $metadata->getBasename($metadata->getExtension()) . (count($langs) > 1 ? "{$lang}." : '') . 'html';
+
+            file_put_contents($path, $output);
+            $this->getArticleEntry()->reloadFiles();
+        }
+
+        //exit;
+        return $this;
     }
 
     /**
@@ -146,9 +321,9 @@ abstract class BaseParser
      * @param string $path XPath expression
      * @param \DOMNode $context Optional context node
      */
-    public function evaluate(string $path, ?\DOMNode $context = null)
+    public function evaluate(string $path, ?DOMNode $context = null, DOMXPath $xpath = null)
     {
-        return $this->_xpath->evaluate($path, $context);
+        return ($xpath ?? $this->_xpath)->evaluate($path, $context);
     }
 
     /**
@@ -156,33 +331,33 @@ abstract class BaseParser
      * The path is expected to target a single node
      *
      * @param string $path XPath expression
-     * @param \DOMNode $context Optional context node
+     * @param DOMNode $context Optional context node
      */
-    public function selectText(string $path, ?\DOMNode $context = null): string
+    public function selectText(string $path, ?DOMNode $context = null, DOMXPath $xpath = null): string
     {
-        return \strip_tags(\trim($this->evaluate("string(${path})", $context)));
+        return strip_tags(trim($this->evaluate("string({$path})", $context, $xpath)));
     }
 
     /**
      * Retrieves the nodes that match the given XPath expression
      *
      * @param string $path XPath expression
-     * @param \DOMNode $context Optional context node
+     * @param DOMNode $context Optional context node
      */
-    public function select(string $path, ?\DOMNode $context = null): \DOMNodeList
+    public function select(string $path, ?DOMNode $context = null, DOMXPath $xpath = null): DOMNodeList
     {
-        return $this->_xpath->query($path, $context);
+        return ($xpath ?? $this->_xpath)->query($path, $context);
     }
 
     /**
      * Query the given XPath expression and retrieves the first item
      *
      * @param string $path XPath expression
-     * @param \DOMNode $context Optional context node
+     * @param DOMNode $context Optional context node
      */
-    public function selectFirst(string $path, ?\DOMNode $context = null): ?object
+    public function selectFirst(string $path, ?DOMNode $context = null, DOMXPath $xpath = null): ?object
     {
-        return $this->select($path, $context)->item(0);
+        return $this->select($path, $context, $xpath)->item(0);
     }
 
     /**
